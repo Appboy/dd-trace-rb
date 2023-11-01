3f4626202fa99146d4b2effc6c5e788edecf24e8
Matthew Zinke <matthew.zinke@braze.com>
2023-11-01 13:21:00 -0700

    remove calls to present?

--- lib/datadog/tracing/contrib/redis/instrumentation.rb
+++ lib/datadog/tracing/contrib/redis/instrumentation.rb
@@ -33,7 +33,7 @@ module Datadog
                   )
                 end
 
-                if Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER].present?
+                if !Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER].nil?
                   span.set_tag(
                     Contrib::Redis::Ext::METRIC_CODEOWNER,
                     Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER]
@@ -64,14 +64,14 @@ module Datadog
                 ### BRAZE MODIFICATION
                 span.set_metric Contrib::Redis::Ext::METRIC_RAW_COMMAND_LEN, args.to_s.length
 
-                if Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_FILEPATH].present?
+                if !Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_FILEPATH].nil?
                   span.set_tag(
                     Contrib::Redis::Ext::METRIC_FILEPATH,
                     Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_FILEPATH]
                   )
                 end
 
-                if Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER].present?
+                if !Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER].nil?
                   span.set_tag(
                     Contrib::Redis::Ext::METRIC_CODEOWNER,
                     Thread.current[Contrib::Redis::Ext::THREAD_GLOBAL_CODEOWNER]
